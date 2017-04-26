1965a63cc14cc01eb2730a9d907f2ef05cd36db9
Atanas Mirchev <taimir93@gmail.com>
2017-04-26 15:32:55 +0200

    Add installation script, update docu

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # keras-layers
 Deep learning layers for different applications; written in `keras`
+
+## Installation
+
+```
+pip install -r requirements.txt
+python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='Keras-layers',
+      version='0.0.1',
+      description='Collection of useful non-standard layers for keras',
+      author='Atanas Mirchev',
+      author_email='taimir93@gmail.com',
+      url='https://github.com/taimir/keras-layers',
+      license='MIT',
+      packages=find_packages())
